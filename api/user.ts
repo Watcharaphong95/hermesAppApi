fd9acf0749592f62151703ef91d5bd4c46b92888
Watcharaphong <65011212077@msu.ac.th>
2024-10-07 22:24:38 +0700

    merge rider and user

--- api/user.ts
+++ api/user.ts
@@ -2,24 +2,12 @@ import express from "express";
 import { conn } from "../dbconnect";
 import mysql from "mysql";
 import { UserRegisterReq } from "../models/Request/user_register_req";
+import { RiderRegisterReq } from "../models/Request/rider_register_req";
 
 export const router = express.Router();
 
-// select all user
-router.get('/', (req, res) => {
-    let sql = 'SELECT * FROM user';
-
-    conn.query(sql, (err, result) => {
-        if(err) {
-            res.status(400).json({msg: err.message});
-        } else {
-            res.json({result});
-        }
-    })
-});
-
 // select user by uid
-router.get('/:uid', (req, res) => {
+router.get('/customer/:uid', (req, res) => {
     let uid = req.params.uid;
 
     let sql = 'SELECT * FROM user WHERE uid = ?'
@@ -38,7 +26,7 @@ router.get('/:uid', (req, res) => {
 });
 
 // select user by some phone
-router.get('/search/:phone', (req, res) => {
+router.get('/customer/search/:phone', (req, res) => {
     let phone = req.params.phone;
     let sql = 'SELECT * FROM user WHERE phone LIKE ?';
 
@@ -56,7 +44,7 @@ router.get('/search/:phone', (req, res) => {
 });
 
 // register for user
-router.post('/register', (req, res)=>{
+router.post('/registerUser', (req, res)=>{
     let users: UserRegisterReq = req.body;
 
     let sql = "INSERT INTO user (phone, name, password, address, lat, lng, picture) VALUES (?,?,?,?,?,?,?)";
@@ -78,9 +66,37 @@ router.post('/register', (req, res)=>{
     })
 });
 
+// register for user
+router.post('/registerRider', (req, res)=>{
+    let riders: RiderRegisterReq = req.body;
+
+    let sql = "INSERT INTO user (phone, name, password, picture, plate, type) VALUES (?,?,?,?,?,?)";
+    sql = mysql.format(sql, [
+        riders.phone,
+        riders.name,
+        riders.password,
+        riders.picture,
+        riders.plate
+    ]);
+    conn.query(sql, (err, result) => {
+        if(err) {
+            res.status(400).json({msg: err.message});
+        } else {
+            res.json({affected_rows: result.affectedRows, last_idx: result.insertId});
+        }
+    })
+});
+
 // Get user
-router.get("/", (req, res) => {
+router.get("/customer", (req, res) => {
     conn.query('SELECT * FROM user', (err, result, fields)=>{
+      res.json(result);
+    });
+  });
+
+// Get user
+router.get("/rider", (req, res) => {
+    conn.query('SELECT * FROM user WHERE type != 1', (err, result, fields)=>{
       res.json(result);
     });
   });
